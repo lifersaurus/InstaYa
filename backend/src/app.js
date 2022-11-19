e5c5b3399a60ebbf73cbdd751b93af5e21c818ee
lifersaurus <linaguerreroomg@gmail.com>
2022-11-19 10:55:07 -0500

    4

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express= require('express');
 const app= express();
 
 //configuraciones 
-app.set('port', 4000);
+app.set('port', 7000);
 
 
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const session=require('express-session');
 const app=express();
 require('./database');
 //confi
-app.set('port', process.env.PORT||3000 );
+app.set('port', process.env.PORT||7000 );
 app.set('views', path.join(__dirname,'views') );
 app.engine('.hbs', exphbs.engine({
     defaultLayout:'main',
